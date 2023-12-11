1e3c64d263f74701b10e988fbf18a5d43abafbd9
HumbleBird <wltnsdid67@naver.com>
2023-12-11 15:13:51 +0900

    유니티 연동 #2

--- Assets/005_Rookiss_GameServer/Packet/PacketHandler.cs
+++ Assets/005_Rookiss_GameServer/Packet/PacketHandler.cs
@@ -8,15 +8,20 @@ using UnityEngine;
 
 class PacketHandler
 {
-
     public static void S_ChatHandler(PacketSession session, IPacket packet)
     {
         S_Chat chatPacket = packet as S_Chat;
         ServerSession serverSession = session as ServerSession;
 
-        if(chatPacket.playerId == 1)
-            Debug.Log(chatPacket.chat);
         //if(chatPacket.playerId == 1)
-        // Console.WriteLine(chatPacket.chat);
+        {
+            Debug.Log(chatPacket.chat);
+
+            GameObject go = GameObject.Find("Player");
+            if (go == null)
+                Debug.Log("Player Not Found");
+            else
+                Debug.Log("Player Found");
+        }
     }
 }
